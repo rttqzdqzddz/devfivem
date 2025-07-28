e61b29ac9b3fbe11cdbc3dea40f77cf80e22355b
rttqzdqzddz <flaashhhdev@gmail.com>
2025-07-28 02:51:05 +0200

    Add ESX jetski rental script

---
+++ jetski/config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.RentalLocation = vector3(-342.1, -1000.2, 24.0)
+Config.SpawnLocation = vector4(-347.9, -992.6, 0.0, 180.0)
+Config.Price = 500
---
+++ jetski/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Example'
+description 'Jetski Rental Script for ESX'
+
+server_script '@es_extended/locale.lua'
+server_script 'config.lua'
+server_script 'server/main.lua'
+
+client_script 'config.lua'
+client_script 'client/main.lua'
